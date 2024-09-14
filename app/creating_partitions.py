53b2ca7b724adbfb3bf7114cc21f7f6bfb4276c3
gautam <gaitbbjcdcn@gamil.com>
2024-09-15 02:03:06 +0530

    commiting first time

--- app/creating_partitions.py
+++ app/creating_partitions.py
@@ -7,6 +7,9 @@ from methods.mapping_file import *
 
 
 def write_partitioned_data(partitioned_data, output_path):
+    """
+    This function is used to create partitions
+    """
     try:
         # working_dir = str(os.getcwd()+'/').replace("\\", "/")
         # print(f"Your Working Directory is: {working_dir}")
@@ -23,31 +26,29 @@ def write_partitioned_data(partitioned_data, output_path):
                 writer.writerows(partitioned_data[file_names])
 
     except Exception as e:
-        raise Exception("Error has occured at create_csv_file", e)
+        raise Exception("Error has occured at write_partitioned_data", e)
 
 def partitioning_data_based_on_column(file_path, partitioned_column, output_path):
+    """
+    This function is used to partition the data
+    """
 
-    with open(file_path, newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        # print(reader.json())
-        creating_partition = {}
-        for row in reader:
-            sys.stdout.write(str(row))
-            # print("outside",row["measInfoId"])
-            if row[partitioned_column] not in creating_partition.keys():
-                # print("onlu one",creating_partition)
-                creating_partition[row[partitioned_column]] = [copy.deepcopy(row)]
-                # print("onlu one1",creating_partition)
-                # print("this blovk will run only 1 time",row["measInfoId"])
-                # print(row["measInfoId"])
-            else:
-                # print("we are here ", row)
-                # print("23 one",creating_partition)
-                # print(creating_partition[row["measInfoId"]])
-                # print("id", str(row[partitioned_column]))
-                # print("inside", (creating_partition[str(row[partitioned_column])]))
-                (creating_partition[str(row[partitioned_column])]).append(copy.deepcopy(row))
-            # break
-            # print(creating_partition[row["measInfoId"]])
-        # print(creating_partition)
-    write_partitioned_data(creating_partition, output_path)
+    try:
+
+        with open(file_path, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            # print(reader.json())
+            creating_partition = {}
+            for row in reader:
+                sys.stdout.write(str(row))
+
+                if row[partitioned_column] not in creating_partition.keys():
+
+                    creating_partition[row[partitioned_column]] = [row]
+
+                else:
+                    (creating_partition[str(row[partitioned_column])]).append(row)
+
+        write_partitioned_data(creating_partition, output_path)
+    except Exception as e:
+        raise Exception("Error has occured at partitioning_data_based_on_column", e)
